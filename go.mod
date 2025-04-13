5b47911b0698332c9e83e72e46e8b13d97c52157
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2025-04-14 00:22:24 +0900

    Impl sending to influxdb

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/lemolatoon/AHT21BToInfluxDB
 go 1.24.2
 
 require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/influxdata/influxdb-client-go/v2 v2.14.0 // indirect
+	github.com/influxdata/line-protocol v0.0.0-20200327222509-2487e7298839 // indirect
+	github.com/oapi-codegen/runtime v1.0.0 // indirect
+	golang.org/x/net v0.23.0 // indirect
 	periph.io/x/conn/v3 v3.7.2 // indirect
 	periph.io/x/host/v3 v3.8.5 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log"
+	"os"
 	"time"
 
 	"periph.io/x/conn/v3/driver/driverreg"
 	"periph.io/x/conn/v3/i2c"
 	"periph.io/x/conn/v3/i2c/i2creg"
 	"periph.io/x/host/v3"
+
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api/write"
 )
 
 const AHT21BAddr = 0x38
@@ -70,12 +75,77 @@ func read(dev *i2c.Dev) (*AHT21BResult, error) {
 	}, nil
 }
 
+func initClient() (influxdb2.Client, error) {
+	token, found := os.LookupEnv("INFLUXDB_TOKEN")
+	if !found {
+		return nil, fmt.Errorf("INFLUXDB_TOKEN not set")
+	}
+	url, found := os.LookupEnv("INFLUXDB_URL")
+	if !found {
+		url = "http://localhost:8086"
+	}
+	return influxdb2.NewClient(url, token), nil
+}
+
+func initInfo() (InfluxDBInfo, error) {
+	org, found := os.LookupEnv("INFLUXDB_ORG")
+	if !found {
+		org = "lemolatoon"
+	}
+	bucket, found := os.LookupEnv("INFLUXDB_BUCKET")
+	if !found {
+		bucket = "sensor-home"
+	}
+	return InfluxDBInfo{Org: org, Bucket: bucket}, nil
+}
+
+type InfluxDBInfo struct {
+	Org    string
+	Bucket string
+}
+
+func initLocation() *time.Location {
+	loc, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		log.Printf("Failed to laod Asia/Tokyo timezone: %v", err)
+		loc = time.FixedZone("Asia/Tokyo", 9*60*60) // fallback
+	}
+	return loc
+}
+
+func send(client influxdb2.Client, info InfluxDBInfo, loc *time.Location, result *AHT21BResult) {
+	writeAPI := client.WriteAPIBlocking(info.Org, info.Bucket)
+	tags := map[string]string{
+		"sensor": "AHT21B",
+	}
+	fields := map[string]interface{}{
+		"temperature": result.Temperature,
+		"humidity":    result.Humidity,
+	}
+	point := write.NewPoint("sensor_data", tags, fields, time.Now().In(loc))
+
+	if err := writeAPI.WritePoint(context.Background(), point); err != nil {
+		log.Printf("Error writing point: %v", err)
+	}
+}
+
 func main() {
 	dev, closer, err := initDevice()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer closer.Close()
+
+	loc := initLocation()
+	info, err := initInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+	client, err := initClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	for {
 		result, err := read(dev)
 		if err != nil {
@@ -84,5 +154,8 @@ func main() {
 			fmt.Printf("Temperature: %.2f °C\n", result.Temperature)
 			fmt.Printf("Humidity: %.2f %%RH\n", result.Humidity)
 		}
+		go send(client, info, loc, result)
+		// sleep for 5 second
+		time.Sleep(5 * time.Second)
 	}
 }
